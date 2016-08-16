85c361b10e38a9f8e263952ad6422dfa20d07949
Aron Fyodor Asor <aronasorman@gmail.com>
2016-08-16 15:42:44 -0700

    import from future

--- django_q/cluster.py
+++ django_q/cluster.py
@@ -4,7 +4,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from builtins import range
+from future.builtins import range
 
 from future import standard_library
 
